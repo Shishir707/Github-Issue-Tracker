9a9c890298ff5377d93c5470a7a5c78778a5a8b9
Shishir707 <mirshr8@gmail.com>
2026-03-10 14:13:58 +0600

    fixed close button items

--- scripts/items.js
+++ scripts/items.js
@@ -74,7 +74,6 @@ buttons.forEach(button => {
 
 
 // For Open Items
-
 function openData() {
     const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
 
@@ -108,6 +107,69 @@ function openData() {
             };
 
             open.forEach(item => {
+                const itemElement = document.createElement("div");
+                itemElement.classList.add("p-4", "bg-white", "rounded-md", "shadow-md", "gap-4");
+                itemElement.innerHTML =
+                    `<div>
+                    <div class="flex justify-between">
+                        <img src="${imageLinks[item.status]}" alt="open status" class="w-6 h-6 rounded-full" />
+                        <p class="${colors[item.priority]} px-4 rounded-md"">${item.priority.toUpperCase()}</p>
+                    </div>
+
+                    <h2 class="text-xl font-semibold">${item.title}</h2>
+                    <p class="text-gray-600">${item.description}</p>
+                    <div class="flex gap-2 mt-4">
+                    ${item.labels.map(label => {
+                        const color = labelColors[label.toLowerCase()] || "bg-gray-100 text-gray-600";
+                        return `<p class="${color} px-3 rounded-xl shadow-md">${label.toUpperCase()}</p>`;
+                    }).join("")}
+                    </div>                  
+                    <hr class="mt-4 text-gray-400 shadow-md">
+                    <p class="text-sm text-gray-500 mt-2">${item.author}</p>
+                    <p class="text-sm text-gray-500 mt-2">${item.updatedAt}</p>
+                </div>`;
+                itemsContainer.appendChild(itemElement);
+            });
+        });
+}
+
+
+// For Closed Items
+function closedData() {
+    const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+
+            const closed = data.data.filter(item => item.status === "closed");
+
+            const issueCount = document.getElementById("issueCount");
+
+            issueCount.textContent = closed.length;
+
+            console.log(closed);
+
+            itemsContainer.innerHTML = "";
+
+            const colors = {
+                high: "bg-red-100 text-red-600",
+                medium: "bg-yellow-100 text-yellow-600",
+                low: "bg-gray-100 text-gray-600"
+            };
+
+            const imageLinks = {
+                open: "./assets/Open-Status.png",
+                closed: "./assets/Closed- Status .png"
+            };
+
+            const labelColors = {
+                bug: "bg-red-100 text-red-600",
+                "help wanted": "bg-yellow-100 text-yellow-600",
+                enhancement: "bg-green-100 text-green-600"
+            };
+
+            closed.forEach(item => {
                 const itemElement = document.createElement("div");
                 itemElement.classList.add("p-4", "bg-white", "rounded-md", "shadow-md", "gap-4");
                 itemElement.innerHTML =
