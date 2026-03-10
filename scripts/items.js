fd2390061db6338a757397357edaae260cb60a00
Shishir707 <mirshr8@gmail.com>
2026-03-10 14:45:53 +0600

    make dynamic display function & reduce code

--- scripts/items.js
+++ scripts/items.js
@@ -1,6 +1,7 @@
 
 const itemsContainer = document.getElementById("showItems");
 
+
 // For all items loading and displaying
 const loadItems = () => {
     const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
@@ -8,54 +9,8 @@ const loadItems = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => {
-
-            const Items = data.data;
-            const issueCount = document.getElementById("issueCount");
-
-            issueCount.textContent = Items.length;
-
-            const colors = {
-                high: "bg-red-100 text-red-600",
-                medium: "bg-yellow-100 text-yellow-600",
-                low: "bg-gray-100 text-gray-600"
-            };
-
-            const imageLinks = {
-                open: "./assets/Open-Status.png",
-                closed: "./assets/Closed- Status .png"
-            };
-
-            const labelColors = {
-                bug: "bg-red-100 text-red-600",
-                "help wanted": "bg-yellow-100 text-yellow-600",
-                enhancement: "bg-green-100 text-green-600"
-            };
-
-            Items.forEach(item => {
-                const itemElement = document.createElement("div");
-                itemElement.classList.add("p-4", "bg-white", "rounded-md", "shadow-md", "gap-4");
-                itemElement.innerHTML =
-                    `<div>
-                    <div class="flex justify-between">
-                        <img src="${imageLinks[item.status]}" alt="open status" class="w-6 h-6 rounded-full" />
-                        <p class="${colors[item.priority]} px-4 rounded-md"">${item.priority.toUpperCase()}</p>
-                    </div>
-
-                    <h2 class="text-xl font-semibold">${item.title}</h2>
-                    <p class="text-gray-600">${item.description}</p>
-                    <div class="flex gap-2 mt-4">
-                    ${item.labels.map(label => {
-                        const color = labelColors[label.toLowerCase()] || "bg-gray-100 text-gray-600";
-                        return `<p class="${color} px-3 rounded-xl shadow-md">${label.toUpperCase()}</p>`;
-                    }).join("")}
-                    </div>                  
-                    <hr class="mt-4 text-gray-400 shadow-md">
-                    <p class="text-sm text-gray-500 mt-2">${item.author}</p>
-                    <p class="text-sm text-gray-500 mt-2">${item.updatedAt}</p>
-                </div>`;
-                itemsContainer.appendChild(itemElement);
-            });
-
+            const allData = data.data;
+            displayItems(allData);
         });
 };
 
@@ -80,56 +35,8 @@ function openData() {
     fetch(url)
         .then(res => res.json())
         .then(data => {
-
             const open = data.data.filter(item => item.status === "open");
-
-            const issueCount = document.getElementById("issueCount");
-
-            issueCount.textContent = open.length;
-
-            itemsContainer.innerHTML = "";
-
-            const colors = {
-                high: "bg-red-100 text-red-600",
-                medium: "bg-yellow-100 text-yellow-600",
-                low: "bg-gray-100 text-gray-600"
-            };
-
-            const imageLinks = {
-                open: "./assets/Open-Status.png",
-                closed: "./assets/Closed- Status .png"
-            };
-
-            const labelColors = {
-                bug: "bg-red-100 text-red-600",
-                "help wanted": "bg-yellow-100 text-yellow-600",
-                enhancement: "bg-green-100 text-green-600"
-            };
-
-            open.forEach(item => {
-                const itemElement = document.createElement("div");
-                itemElement.classList.add("p-4", "bg-white", "rounded-md", "shadow-md", "gap-4");
-                itemElement.innerHTML =
-                    `<div>
-                    <div class="flex justify-between">
-                        <img src="${imageLinks[item.status]}" alt="open status" class="w-6 h-6 rounded-full" />
-                        <p class="${colors[item.priority]} px-4 rounded-md"">${item.priority.toUpperCase()}</p>
-                    </div>
-
-                    <h2 class="text-xl font-semibold">${item.title}</h2>
-                    <p class="text-gray-600">${item.description}</p>
-                    <div class="flex gap-2 mt-4">
-                    ${item.labels.map(label => {
-                        const color = labelColors[label.toLowerCase()] || "bg-gray-100 text-gray-600";
-                        return `<p class="${color} px-3 rounded-xl shadow-md">${label.toUpperCase()}</p>`;
-                    }).join("")}
-                    </div>                  
-                    <hr class="mt-4 text-gray-400 shadow-md">
-                    <p class="text-sm text-gray-500 mt-2">${item.author}</p>
-                    <p class="text-sm text-gray-500 mt-2">${item.updatedAt}</p>
-                </div>`;
-                itemsContainer.appendChild(itemElement);
-            });
+            displayItems(open);
         });
 }
 
@@ -141,39 +48,50 @@ function closedData() {
     fetch(url)
         .then(res => res.json())
         .then(data => {
-
             const closed = data.data.filter(item => item.status === "closed");
+            displayItems(closed);
+        });
+}
 
-            const issueCount = document.getElementById("issueCount");
 
-            issueCount.textContent = closed.length;
 
-            console.log(closed);
+// Dynamic display Function
+const displayItems = (Items) => {
+    const itemsContainer = document.getElementById("showItems");
+    itemsContainer.innerHTML = "";
 
-            itemsContainer.innerHTML = "";
+    const issueCount = document.getElementById("issueCount");
 
-            const colors = {
-                high: "bg-red-100 text-red-600",
-                medium: "bg-yellow-100 text-yellow-600",
-                low: "bg-gray-100 text-gray-600"
-            };
+    issueCount.textContent = Items.length;
 
-            const imageLinks = {
-                open: "./assets/Open-Status.png",
-                closed: "./assets/Closed- Status .png"
-            };
+    const colors = {
+        high: "bg-red-100 text-red-600",
+        medium: "bg-yellow-100 text-yellow-600",
+        low: "bg-gray-100 text-gray-600"
+    };
 
-            const labelColors = {
-                bug: "bg-red-100 text-red-600",
-                "help wanted": "bg-yellow-100 text-yellow-600",
-                enhancement: "bg-green-100 text-green-600"
-            };
+    const imageLinks = {
+        open: "./assets/Open-Status.png",
+        closed: "./assets/Closed- Status .png"
+    };
 
-            closed.forEach(item => {
-                const itemElement = document.createElement("div");
-                itemElement.classList.add("p-4", "bg-white", "rounded-md", "shadow-md", "gap-4");
-                itemElement.innerHTML =
-                    `<div>
+    const labelColors = {
+        bug: "bg-red-100 text-red-600",
+        "help wanted": "bg-yellow-100 text-yellow-600",
+        enhancement: "bg-green-100 text-green-600"
+    };
+
+    const border = {
+        open: "border-indigo-200 border-t-green-500 border-t-4",
+        closed: "border-indigo-200 border-t-purple-500 border-t-4"
+    }
+
+    Items.forEach(item => {
+        const itemElement = document.createElement("div");
+        itemElement.classList.add("p-4", "bg-white", "rounded-md", "shadow-md", "gap-4");
+        itemElement.classList.add(...border[item.status].split(" "));
+        itemElement.innerHTML =
+            `<div>
                     <div class="flex justify-between">
                         <img src="${imageLinks[item.status]}" alt="open status" class="w-6 h-6 rounded-full" />
                         <p class="${colors[item.priority]} px-4 rounded-md"">${item.priority.toUpperCase()}</p>
@@ -183,15 +101,14 @@ function closedData() {
                     <p class="text-gray-600">${item.description}</p>
                     <div class="flex gap-2 mt-4">
                     ${item.labels.map(label => {
-                        const color = labelColors[label.toLowerCase()] || "bg-gray-100 text-gray-600";
-                        return `<p class="${color} px-3 rounded-xl shadow-md">${label.toUpperCase()}</p>`;
-                    }).join("")}
+                const color = labelColors[label.toLowerCase()] || "bg-gray-100 text-gray-600";
+                return `<p class="${color} px-3 rounded-xl shadow-md">${label.toUpperCase()}</p>`;
+            }).join("")}
                     </div>                  
                     <hr class="mt-4 text-gray-400 shadow-md">
                     <p class="text-sm text-gray-500 mt-2">${item.author}</p>
                     <p class="text-sm text-gray-500 mt-2">${item.updatedAt}</p>
                 </div>`;
-                itemsContainer.appendChild(itemElement);
-            });
-        });
+        itemsContainer.appendChild(itemElement);
+    });
 }
